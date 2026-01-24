85bb5afbaedc785dbceaf539116c13e59c17b8d7
Satone <kukyo999999999@gmail.com>
2026-01-24 18:31:09 +0900

    fix

--- common/header.js
+++ common/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://satonelab.com/common/header.html")
+  fetch("https://satonelab.com/common/header.html?v=" + Date.now())
     .then(res => res.text())
     .then(html => {
       document.getElementById("header").innerHTML = html;
