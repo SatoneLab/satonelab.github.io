8e6d84cdbc6382daacba2aa187e27b02cd119c3f
Satone <kukyo999999999@gmail.com>
2026-01-26 18:07:27 +0900

    fix

--- common/footer.js
+++ common/footer.js
@@ -1,19 +1,21 @@
 // footer.js
-const BASE = isGAS() ? "https://satonelab.com" : "";  // 環境に応じて自動変更される
+  (function(){
+  const BASE = isGAS() ? "https://satonelab.com" : "";  // 環境に応じて自動変更される
 
-// 開いているページがGASかどうか判定する
-function isGAS() {
-  const host = window.location.hostname;
-  // GAS の Web アプリは googleusercontent.com または script.google.com などのホストになる
-  return host.includes("googleusercontent.com") || host.includes("script.google.com");
-}
+  // 開いているページがGASかどうか判定する
+  function isGAS() {
+    const host = window.location.hostname;
+    // GAS の Web アプリは googleusercontent.com または script.google.com などのホストになる
+    return host.includes("googleusercontent.com") || host.includes("script.google.com");
+  }
 
-window.addEventListener("load", () => {
-  const target = document.getElementById("footer");
-  if (!target) return;
+  window.addEventListener("load", () => {
+    const target = document.getElementById("footer");
+    if (!target) return;
 
-  fetch(`${BASE}/common/footer.html?v=2` + Date.now())
-    .then(res => res.text())
-    .then(html => target.innerHTML = html)
-    .catch(err => console.error("Footer fetch error:", err));
+    fetch(`${BASE}/common/footer.html?v=2` + Date.now())
+      .then(res => res.text())
+      .then(html => target.innerHTML = html)
+      .catch(err => console.error("Footer fetch error:", err));
+  });
 });
