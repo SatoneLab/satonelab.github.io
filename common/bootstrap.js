7954fbf47161331ff5251ddf29686005a203685d
Satone <kukyo999999999@gmail.com>
2026-01-26 18:19:00 +0900

    Update bootstrap.js

--- common/bootstrap.js
+++ common/bootstrap.js
@@ -1,5 +1,5 @@
 const BASE = isGAS() ? "https://satonelab.com" : ""; // 環境に応じて自動変更される
-const VERSION = "2026-01-24-4";
+const VERSION = "2026-01-24-5";
 
 // CSS 読み込み関数
 function css(href, attrs = {}) {
